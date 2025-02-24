f21f9f80c289acad061dee3885b3b6a7efc07c96
Davide De Pasquale <davide.depasquale1998@gmail.com>
2025-02-24 15:48:20 +0100

    login ......

--- src/main/java/com/epicode/GestEventi2_0/security/SecurityConfig.java
+++ src/main/java/com/epicode/GestEventi2_0/security/SecurityConfig.java
@@ -11,7 +11,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,10 +25,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
     @Autowired
     JwtAuthenticationFilter jwtAuthenticationFilter;
-
 
     //csrf -> difende progetto da attacchi esterni (facciamo disable() perchè scriviamo App Restfull)
     // auth x le autenticazioni
@@ -54,7 +58,6 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-
 
     @Bean
     public UserDetailsService userDetailsService(UtenteRepository utenteRepository) {
